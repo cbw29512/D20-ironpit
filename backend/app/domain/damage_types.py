84e2994d8abbf1ffed617d7aa1a7d7352770d088
Chris Wilson <divclass01@gmail.com>
2026-08-27 11:06:05 -0400

    refactor: split damage types from combatants

---
+++ backend/app/domain/damage_types.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+
+
+class DamageType(StrEnum):
+    SLASHING = "slashing"
+    PIERCING = "piercing"
+    BLUDGEONING = "bludgeoning"
+    POISON = "poison"
+    RADIANT = "radiant"
+    NECROTIC = "necrotic"
+    FIRE = "fire"
+    PSYCHIC = "psychic"
