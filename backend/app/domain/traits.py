cfb57be3a85af2a7b050f480c04c4478b66d85e4
Chris Wilson <divclass01@gmail.com>
2026-08-28 23:31:26 -0400

    feat: add Orc combat traits

--- backend/app/domain/traits.py
+++ backend/app/domain/traits.py
@@ -4,3 +4,5 @@ from enum import StrEnum
 class CombatTrait(StrEnum):
     PACK_TACTICS = "pack-tactics"
     SAVAGE_ATTACKER = "savage-attacker"
+    ADRENALINE_RUSH = "adrenaline-rush"
+    RELENTLESS_ENDURANCE = "relentless-endurance"
