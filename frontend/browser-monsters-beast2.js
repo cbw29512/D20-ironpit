fe689f2c775ff4353f908dedafeb127498e1156e
Chris Wilson <divclass01@gmail.com>
2026-08-29 12:25:07 -0400

    feat: mirror five beasts in browser runtime [skip ci]

--- frontend/browser-monsters-beast2.js
+++ frontend/browser-monsters-beast2.js
@@ -18,47 +18,24 @@
   });
 
   const items = [
-    monster("srd-eagle", "Eagle", "0", "small", 12, 4, 60, 2, [
-      attack("eagle-talons", "Talons", 4, 1, 4, 2, "slashing"),
-    ]),
-    monster("srd-panther", "Panther", "1/4", "medium", 13, 13, 50, 3, [
-      attack("panther-rend", "Rend", 5, 1, 6, 3, "slashing"),
-    ]),
-    monster("srd-plesiosaurus", "Plesiosaurus", "2", "large", 13, 68, 20, 2, [
-      attack("plesiosaurus-bite", "Bite", 6, 2, 6, 4, "piercing", { reach: 10 }),
-    ]),
-    monster("srd-polar-bear", "Polar Bear", "2", "large", 12, 42, 40, 2, [
-      attack("polar-bear-rend", "Rend", 7, 1, 8, 5, "slashing"),
-    ], { damage_resistances: ["cold"], attack_action: { id: "srd-polar-bear-multiattack", slots: [["polar-bear-rend"], ["polar-bear-rend"]] } }),
-    monster("srd-pony", "Pony", "1/8", "medium", 10, 11, 40, 0, [
-      attack("pony-hooves", "Hooves", 4, 1, 4, 2, "bludgeoning"),
-    ]),
-    monster("srd-pteranodon", "Pteranodon", "1/4", "medium", 13, 13, 60, 2, [
-      attack("pteranodon-bite", "Bite", 4, 1, 8, 2, "piercing"),
-    ]),
-    monster("srd-riding-horse", "Riding Horse", "1/4", "large", 11, 13, 60, 1, [
-      attack("riding-horse-hooves", "Hooves", 5, 1, 8, 3, "bludgeoning"),
-    ]),
-    monster("srd-tiger", "Tiger", "1", "large", 13, 30, 40, 3, [
-      attack("tiger-rend", "Rend", 5, 2, 6, 3, "slashing", { proneMaxSize: "large" }),
-    ]),
-    monster("srd-vulture", "Vulture", "0", "medium", 10, 5, 50, 0, [
-      attack("vulture-beak", "Beak", 2, 1, 4, 0, "piercing"),
-    ], { traits: ["pack-tactics"] }),
-    monster("srd-giant-fire-beetle", "Giant Fire Beetle", "0", "small", 13, 4, 30, 0, [
-      attack("giant-fire-beetle-bite", "Bite", 1, 0, 2, 0, "fire", { fixedDamage: 1 }),
-    ], { damage_resistances: ["fire"] }),
-    monster("srd-giant-goat", "Giant Goat", "1/2", "large", 11, 19, 40, 1, [
-      attack("giant-goat-ram", "Ram", 5, 1, 6, 3, "bludgeoning", {
-        charge: charge(20, 2, 4, "bludgeoning", "large"),
-      }),
-    ], { traits: ["charge"] }),
-    monster("srd-giant-owl", "Giant Owl", "1/4", "large", 12, 19, 60, 2, [
-      attack("giant-owl-talons", "Talons", 4, 1, 10, 2, "slashing"),
-    ], { damage_resistances: ["necrotic", "radiant"] }),
-    monster("srd-hyena", "Hyena", "0", "medium", 11, 5, 50, 1, [
-      attack("hyena-bite", "Bite", 2, 1, 6, 0, "piercing"),
-    ], { traits: ["pack-tactics"] }),
+    monster("srd-eagle", "Eagle", "0", "small", 12, 4, 60, 2, [attack("eagle-talons", "Talons", 4, 1, 4, 2, "slashing")]),
+    monster("srd-panther", "Panther", "1/4", "medium", 13, 13, 50, 3, [attack("panther-rend", "Rend", 5, 1, 6, 3, "slashing")]),
+    monster("srd-plesiosaurus", "Plesiosaurus", "2", "large", 13, 68, 20, 2, [attack("plesiosaurus-bite", "Bite", 6, 2, 6, 4, "piercing", { reach: 10 })]),
+    monster("srd-polar-bear", "Polar Bear", "2", "large", 12, 42, 40, 2, [attack("polar-bear-rend", "Rend", 7, 1, 8, 5, "slashing")], { damage_resistances: ["cold"], attack_action: { id: "srd-polar-bear-multiattack", slots: [["polar-bear-rend"], ["polar-bear-rend"]] } }),
+    monster("srd-pony", "Pony", "1/8", "medium", 10, 11, 40, 0, [attack("pony-hooves", "Hooves", 4, 1, 4, 2, "bludgeoning")]),
+    monster("srd-pteranodon", "Pteranodon", "1/4", "medium", 13, 13, 60, 2, [attack("pteranodon-bite", "Bite", 4, 1, 8, 2, "piercing")]),
+    monster("srd-riding-horse", "Riding Horse", "1/4", "large", 11, 13, 60, 1, [attack("riding-horse-hooves", "Hooves", 5, 1, 8, 3, "bludgeoning")]),
+    monster("srd-tiger", "Tiger", "1", "large", 13, 30, 40, 3, [attack("tiger-rend", "Rend", 5, 2, 6, 3, "slashing", { proneMaxSize: "large" })]),
+    monster("srd-vulture", "Vulture", "0", "medium", 10, 5, 50, 0, [attack("vulture-beak", "Beak", 2, 1, 4, 0, "piercing")], { traits: ["pack-tactics"] }),
+    monster("srd-giant-fire-beetle", "Giant Fire Beetle", "0", "small", 13, 4, 30, 0, [attack("giant-fire-beetle-bite", "Bite", 1, 0, 2, 0, "fire", { fixedDamage: 1 })], { damage_resistances: ["fire"] }),
+    monster("srd-giant-goat", "Giant Goat", "1/2", "large", 11, 19, 40, 1, [attack("giant-goat-ram", "Ram", 5, 1, 6, 3, "bludgeoning", { charge: charge(20, 2, 4, "bludgeoning", "large") })], { traits: ["charge"] }),
+    monster("srd-giant-owl", "Giant Owl", "1/4", "large", 12, 19, 60, 2, [attack("giant-owl-talons", "Talons", 4, 1, 10, 2, "slashing")], { damage_resistances: ["necrotic", "radiant"] }),
+    monster("srd-hyena", "Hyena", "0", "medium", 11, 5, 50, 1, [attack("hyena-bite", "Bite", 2, 1, 6, 0, "piercing")], { traits: ["pack-tactics"] }),
+    monster("srd-giant-bat", "Giant Bat", "1/4", "large", 13, 22, 60, 3, [attack("giant-bat-bite", "Bite", 5, 1, 6, 3, "piercing")]),
+    monster("srd-mastiff", "Mastiff", "1/8", "medium", 12, 5, 40, 2, [attack("mastiff-bite", "Bite", 3, 1, 6, 1, "piercing", { proneMaxSize: "medium" })]),
+    monster("srd-mule", "Mule", "1/8", "medium", 10, 11, 40, 0, [attack("mule-hooves", "Hooves", 4, 1, 4, 2, "bludgeoning")]),
+    monster("srd-rhinoceros", "Rhinoceros", "2", "large", 13, 45, 40, -1, [attack("rhinoceros-gore", "Gore", 7, 2, 8, 5, "piercing", { charge: charge(20, 2, 8, "piercing", "large") })], { traits: ["charge"] }),
+    monster("srd-warhorse", "Warhorse", "1/2", "large", 11, 19, 60, 1, [attack("warhorse-hooves", "Hooves", 6, 2, 4, 4, "bludgeoning", { charge: charge(20, 2, 4, "bludgeoning", "large") })], { traits: ["charge"] }),
   ];
 
   Object.assign(window.IRON_PIT_BROWSER_MONSTERS, Object.fromEntries(items.map((item) => [item.id, item])));
