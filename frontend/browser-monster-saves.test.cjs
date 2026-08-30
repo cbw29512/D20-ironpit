36ffae62f3670b425d6a43b4e43a74dcbc855fdf
Chris Wilson <divclass01@gmail.com>
2026-08-30 17:59:01 -0400

    test: certify six saves in generated browser monsters

---
+++ frontend/browser-monster-saves.test.cjs
@@ -0,0 +1,17 @@
+"use strict";
+
+const assert = require("node:assert/strict");
+const fs = require("node:fs");
+const path = require("node:path");
+const vm = require("node:vm");
+
+global.window = globalThis;
+vm.runInThisContext(fs.readFileSync(path.join(__dirname, "browser-monsters-generated.js"), "utf8"));
+const expected = ["charisma", "constitution", "dexterity", "intelligence", "strength", "wisdom"];
+const monsters = Object.values(window.IRON_PIT_BROWSER_MONSTERS);
+assert.equal(monsters.length, 68);
+for (const monster of monsters) {
+  assert.deepEqual(Object.keys(monster.saving_throw_bonuses || {}).sort(), expected, `${monster.name} must expose all six certified saves`);
+  for (const value of Object.values(monster.saving_throw_bonuses)) assert.equal(Number.isInteger(value), true, `${monster.name} saves must be integers`);
+}
+console.log("Generated browser monsters expose all six SRD saving throws.");
