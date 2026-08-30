118dff91e67c9af969dce2497fc22317a5c5245e
Chris Wilson <divclass01@gmail.com>
2026-08-30 16:14:26 -0400

    Grant browser Petrified immunity to Poisoned

--- frontend/browser-condition-immunity.js
+++ frontend/browser-condition-immunity.js
@@ -2,7 +2,8 @@
   "use strict";
 
   function immune(state, conditionId) {
-    return state.template.condition_immunities?.includes(conditionId) === true;
+    if (state.template.condition_immunities?.includes(conditionId) === true) return true;
+    return conditionId === "poisoned" && state.active_effect_ids.includes("petrified");
   }
 
   window.IRON_PIT_BROWSER_CONDITION_IMMUNITY = { immune };
