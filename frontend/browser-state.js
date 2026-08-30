7e31de915eaf0f4ab7515dcdb74378de50a7b2e5
Chris Wilson <divclass01@gmail.com>
2026-08-30 15:57:23 -0400

    Track browser spell-slot use by active turn

--- frontend/browser-state.js
+++ frontend/browser-state.js
@@ -13,6 +13,7 @@
       action_available: true, bonus_action_available: true, reaction_available: true,
       movement_remaining_ft: 0, resources: { ...(template.resources || {}) },
       active_effect_ids: [], grapple_sources: [], timed_effects: [], feature_last_turn_keys: {},
+      spell_slot_expended_turn_key: null,
       temporary_damage_resistances: [], rage_expires_round: null, rage_max_round: null,
     };
   }
