1a16c473ab46dc6cc68aa6f995ff469dd70296ea
Chris Wilson <divclass01@gmail.com>
2026-08-31 11:52:27 -0400

    feat: submit five zero-engine monsters for full RAW certification

--- backend/app/content/monster_catalog.py
+++ backend/app/content/monster_catalog.py
@@ -48,10 +48,12 @@ _READY_BY_NAME = {
     "Vulture": "srd-vulture", "Warhorse": "srd-warhorse", "Warrior Infantry": "srd-warrior-infantry",
     "Weasel": "srd-weasel", "Wolf": "srd-wolf",
     "Animated Armor": "srd-animated-armor", "Animated Flying Sword": "srd-animated-flying-sword",
-    "Flying Snake": "srd-flying-snake", "Hippopotamus": "srd-hippopotamus",
-    "Killer Whale": "srd-killer-whale", "Manticore": "srd-manticore", "Pegasus": "srd-pegasus",
-    "Scorpion": "srd-scorpion", "Skeleton": "srd-skeleton", "Spider": "srd-spider",
-    "Tough": "srd-tough", "Venomous Snake": "srd-venomous-snake",
+    "Awakened Tree": "srd-awakened-tree", "Flying Snake": "srd-flying-snake",
+    "Gargoyle": "srd-gargoyle", "Grimlock": "srd-grimlock", "Guard Captain": "srd-guard-captain",
+    "Hippopotamus": "srd-hippopotamus", "Killer Whale": "srd-killer-whale",
+    "Manticore": "srd-manticore", "Pegasus": "srd-pegasus", "Scorpion": "srd-scorpion",
+    "Skeleton": "srd-skeleton", "Spider": "srd-spider", "Tough": "srd-tough",
+    "Venomous Snake": "srd-venomous-snake", "Violet Fungus": "srd-violet-fungus",
 }
 
 
