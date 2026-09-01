d5b81c917891f57a08c702f289f1dc2f5aab326b
Chris Wilson <divclass01@gmail.com>
2026-09-01 04:39:07 -0400

    build: export certified Shield of Faith data

---
+++ frontend/browser-spell-effects.js
@@ -0,0 +1,6 @@
+/* GENERATED from canonical Python certified spell effects. Do not hand-edit. */
+(() => {
+  "use strict";
+  const spells = [{"actionCost":"bonus_action","animation":"shield-of-faith","concentration":true,"damageResistances":[],"durationMinutes":10,"id":"shield-of-faith","level":1,"modifierEffects":[{"damageType":null,"diceCount":0,"diceSize":0,"flatBonus":2,"kind":"armor-class"}],"name":"Shield of Faith","priority":20,"range":60,"source":"SRD 5.2.1 p.162","temporaryHp":0,"temporaryHpPerSlotAbove":0}];
+  window.IRON_PIT_BROWSER_SPELL_EFFECTS = Object.fromEntries(spells.map((item) => [item.id, item]));
+})();
