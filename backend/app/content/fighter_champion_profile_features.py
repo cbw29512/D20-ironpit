790d31534b7f3d0985afb122fae184f1935f4bfb
Chris Wilson <divclass01@gmail.com>
2026-09-02 10:34:28 -0400

    feat: mark Champion Tactical Master automated

--- backend/app/content/fighter_champion_profile_features.py
+++ backend/app/content/fighter_champion_profile_features.py
@@ -27,7 +27,7 @@ _FEATURES = (
     ProfileFeature(5, "extra-attack", "Extra Attack", "class", True),
     ProfileFeature(5, "tactical-shift", "Tactical Shift", "class", True),
     ProfileFeature(9, "indomitable", "Indomitable", "class", True),
-    ProfileFeature(9, "tactical-master", "Tactical Master", "class", False),
+    ProfileFeature(9, "tactical-master", "Tactical Master", "class", True),
     ProfileFeature(10, "heroic-warrior", "Heroic Warrior", "subclass", False),
     ProfileFeature(11, "two-extra-attacks", "Two Extra Attacks", "class", True),
     ProfileFeature(13, "studied-attacks", "Studied Attacks", "class", False),
