a4521d58c471d4d265e0a6699ef604e2868803b5
Chris Wilson <divclass01@gmail.com>
2026-09-05 13:47:02 -0400

    Load shared state in Fighter 4 browser harness

--- frontend/browser-fighter4.test.cjs
+++ frontend/browser-fighter4.test.cjs
@@ -13,6 +13,7 @@ const load = (name) => vm.runInThisContext(
 
 load("browser-heroes.js");
 load("browser-action-economy.js");
+load("browser-state.js");
 load("browser-support.js");
 
 const fighter3 = window.IRON_PIT_BROWSER_HEROES["karnok-stoneward-l3"];
