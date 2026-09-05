0ce13638dcf999a9305c6753a09d774f359e96bf
Chris Wilson <divclass01@gmail.com>
2026-09-05 19:49:29 -0400

    test: lock figure profiles for promoted monsters

--- backend/tests/test_figure_profiles.py
+++ backend/tests/test_figure_profiles.py
@@ -26,9 +26,10 @@ def test_reviewed_profiles_are_explicit_and_nonempty() -> None:
 def test_anatomically_distinct_monsters_do_not_share_humanoid_fallbacks() -> None:
     expected = {
         "Animated Flying Sword": "weapon", "Ankylosaurus": "reptile", "Archelon": "aquatic-reptile",
-        "Axe Beak": "bird", "Baboon": "primate", "Blood Hawk": "bird", "Flying Snake": "snake",
-        "Gargoyle": "gargoyle", "Giant Constrictor Snake": "snake", "Giant Crocodile": "reptile",
-        "Giant Eagle": "bird", "Giant Elk": "hoofed", "Giant Wolf Spider": "spider", "Goat": "hoofed",
+        "Axe Beak": "bird", "Baboon": "primate", "Blink Dog": "quadruped", "Blood Hawk": "bird",
+        "Fire Giant": "brute", "Flying Snake": "snake", "Gargoyle": "gargoyle",
+        "Giant Constrictor Snake": "snake", "Giant Crocodile": "reptile", "Giant Eagle": "bird",
+        "Giant Elk": "hoofed", "Giant Wolf Spider": "spider", "Goat": "hoofed",
         "Green Dragon Wyrmling": "reptile", "Hippogriff": "hippogriff", "Hippopotamus": "quadruped",
         "Killer Whale": "aquatic-mammal", "Lemure": "brute", "Manticore": "quadruped",
         "Minotaur Skeleton": "brute", "Owlbear": "bear", "Pegasus": "hoofed",
@@ -36,7 +37,7 @@ def test_anatomically_distinct_monsters_do_not_share_humanoid_fallbacks() -> Non
         "Rhinoceros": "hoofed", "Scorpion": "scorpion", "Spider": "spider", "Swarm of Bats": "swarm",
         "Swarm of Crawling Claws": "swarm", "Swarm of Insects": "swarm", "Swarm of Rats": "swarm",
         "Swarm of Venomous Snakes": "swarm", "Triceratops": "reptile",
-        "Tyrannosaurus Rex": "theropod", "Worg": "quadruped",
+        "Tyrannosaurus Rex": "theropod", "Worg": "quadruped", "Xorn": "brute",
     }
     profiles = _profiles()
     for name, form in expected.items():
@@ -46,15 +47,17 @@ def test_anatomically_distinct_monsters_do_not_share_humanoid_fallbacks() -> Non
 def test_new_batch_has_specific_reviewed_details() -> None:
     expected = {
         "Animated Armor": "animated-armor", "Animated Flying Sword": "flying-sword", "Awakened Tree": "tree",
-        "Blood Hawk": "blood-hawk", "Flying Snake": "flying-snake", "Gargoyle": "gargoyle", "Goat": "goat",
+        "Blink Dog": "blink-dog", "Blood Hawk": "blood-hawk", "Fire Giant": "fire-giant",
+        "Flying Snake": "flying-snake", "Gargoyle": "gargoyle", "Goat": "goat",
         "Goblin Boss": "goblin-boss", "Green Dragon Wyrmling": "green-dragon-wyrmling",
         "Grimlock": "grimlock", "Guard Captain": "guard-captain", "Hippopotamus": "hippopotamus",
         "Killer Whale": "orca", "Lemure": "lemure", "Manticore": "manticore",
         "Merfolk Skirmisher": "merfolk-skirmisher", "Minotaur Skeleton": "minotaur-skeleton",
         "Pegasus": "pegasus", "Reef Shark": "shark", "Scorpion": "scorpion", "Skeleton": "skeleton",
-        "Swarm of Bats": "bats", "Swarm of Crawling Claws": "crawling-claws", "Swarm of Insects": "insects",
-        "Swarm of Rats": "rats", "Swarm of Venomous Snakes": "venomous-snakes", "Triceratops": "triceratops",
-        "Violet Fungus": "violet-fungus", "Worg": "canine",
+        "Spy": "spy", "Swarm of Bats": "bats", "Swarm of Crawling Claws": "crawling-claws",
+        "Swarm of Insects": "insects", "Swarm of Rats": "rats", "Swarm of Venomous Snakes": "venomous-snakes",
+        "Tough Boss": "tough-boss", "Triceratops": "triceratops", "Violet Fungus": "violet-fungus",
+        "Worg": "canine", "Xorn": "xorn",
     }
     profiles = _profiles()
     for name, detail in expected.items():
