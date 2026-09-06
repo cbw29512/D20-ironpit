ee8ef3af81b45051672663d90d5ae24ec983b637
Chris Wilson <divclass01@gmail.com>
2026-09-06 15:39:22 -0400

    Split aquatic figure profiles

---
+++ backend/app/content/figure_profiles_aquatic.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+AQUATIC_FIGURE_PROFILES = {
+    "Archelon": {"form": "aquatic-reptile", "detail": "archelon"},
+    "Crab": {"form": "crab", "detail": "crab"},
+    "Crocodile": {"form": "reptile", "detail": "crocodile"},
+    "Giant Crab": {"form": "crab", "detail": "crab"},
+    "Giant Crocodile": {"form": "reptile", "detail": "crocodile"},
+    "Giant Shark": {"form": "aquatic-fish", "detail": "shark"},
+    "Hunter Shark": {"form": "aquatic-fish", "detail": "shark"},
+    "Killer Whale": {"form": "aquatic-mammal", "detail": "orca"},
+    "Merfolk Skirmisher": {"form": "humanoid", "detail": "merfolk-skirmisher"},
+    "Piranha": {"form": "aquatic-fish", "detail": "piranha"},
+    "Plesiosaurus": {"form": "aquatic-reptile", "detail": "plesiosaur"},
+    "Reef Shark": {"form": "aquatic-fish", "detail": "shark"},
+    "Sahuagin Warrior": {"form": "humanoid", "detail": "sahuagin-warrior"},
+}
