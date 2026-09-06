51304c2aafac168aaf4bb6d1dd32f43d6f922b50
Chris Wilson <divclass01@gmail.com>
2026-09-05 22:07:17 -0400

    feat: map Death Burst to universal death trigger

--- backend/app/content/monster_trait_source_audit.py
+++ backend/app/content/monster_trait_source_audit.py
@@ -20,6 +20,7 @@ _MODELED_TRAITS = {
     "Magic Resistance": CombatTrait.MAGIC_RESISTANCE,
     "Regeneration": CombatTrait.REGENERATION,
     "Fire Aura": CombatTrait.END_TURN_DAMAGE_AURA,
+    "Death Burst": CombatTrait.DEATH_TRIGGER_SAVE,
     "Swarm": CombatTrait.SWARM,
     "Undead Fortitude": CombatTrait.UNDEAD_FORTITUDE,
 }
