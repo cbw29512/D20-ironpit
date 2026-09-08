acb6d24c93663a934438adc4834072e7790cc353
Chris Wilson <divclass01@gmail.com>
2026-09-08 13:39:36 -0400

    fix: sync Hill Giant unarmed browser fallback

--- frontend/browser-unarmed-opportunity.js
+++ frontend/browser-unarmed-opportunity.js
@@ -7,6 +7,7 @@
     "srd-giant-constrictor-snake": {"attack_bonus":6,"damage":5},
     "srd-giant-elk": {"attack_bonus":6,"damage":5},
     "srd-giant-venomous-snake": {"attack_bonus":2,"damage":1},
+    "srd-hill-giant": {"attack_bonus":8,"damage":6},
     "srd-plesiosaurus": {"attack_bonus":6,"damage":5},
     "srd-tyrannosaurus-rex": {"attack_bonus":10,"damage":8},
     "srd-violet-fungus": {"attack_bonus":-2,"damage":0}
