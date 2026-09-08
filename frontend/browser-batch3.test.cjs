abcaaeaf0fb8230cf2147f3f27f9575fb35f4ed5
Chris Wilson <divclass01@gmail.com>
2026-09-08 19:19:33 -0400

    test: sync batch3 harness with universal attack modules

--- frontend/browser-batch3.test.cjs
+++ frontend/browser-batch3.test.cjs
@@ -10,7 +10,8 @@ const load = (name) => vm.runInThisContext(fs.readFileSync(path.join(__dirname, 
 for (const file of [
   "browser-heroes.js", "browser-monsters.js", "browser-monsters-fixed.js",
   "browser-monsters-beast2.js", "browser-monsters-batch3.js", "browser-state.js",
-  "browser-rage.js", "browser-rolls.js", "browser-zero-hp.js", "browser-attack.js", "browser-charge.js",
+  "browser-rage.js", "browser-rolls.js", "browser-zero-hp.js", "browser-forced-movement.js",
+  "browser-control.js", "browser-attack-helpers.js", "browser-attack.js", "browser-charge.js",
   "browser-formation.js", "browser-multiattack.js", "browser-turn.js", "browser-engine.js",
 ]) load(file);
 
