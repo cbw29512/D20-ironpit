56a4af0307338e499f4a9fa257aadf0509849a7b
Chris Wilson <divclass01@gmail.com>
2026-09-08 14:28:55 -0400

    engine: serialize save-action resource links

--- backend/app/content/capability_from_template.py
+++ backend/app/content/capability_from_template.py
@@ -85,6 +85,7 @@ def _save(action) -> dict[str, object]:
         "id": action.id, "name": action.name, "save_ability": action.save_ability,
         "dc": action.dc, "range_ft": action.range_ft, "target_max_size": action.target_max_size,
         "success_damage": action.success_damage, "animation": action.animation,
+        "resource_id": action.resource_id, "resource_cost": action.resource_cost,
     }
     if action.damage_dice_count:
         result["damage"] = _dice(action.damage_dice_count, action.damage_dice_size, action.damage_bonus)
