c27c6c855806bae714863ff1018d4d1103895efd
Chris Wilson <divclass01@gmail.com>
2026-09-08 13:16:15 -0400

    classify arena-neutral monster traits generically

--- backend/app/content/monster_trait_source_audit.py
+++ backend/app/content/monster_trait_source_audit.py
@@ -18,11 +18,12 @@ _MODELED_TRAITS = {
     "Undead Fortitude": CombatTrait.UNDEAD_FORTITUDE,
 }
 _ARENA_NEUTRAL_TRAITS = frozenset({
-    "Agile", "Amphibious", "Beast of Burden", "Earth Glide", "False Appearance", "Flyby",
-    "Hellish Restoration", "Hold Breath", "Ice Walk", "Illumination", "Incorporeal Movement", "Jumper", "Keen Hearing",
-    "Keen Hearing and Sight", "Keen Hearing and Smell", "Keen Sight", "Keen Smell", "Limited Amphibiousness", "Mimicry",
-    "Running Leap", "Shark Telepathy", "Spider Climb", "Standing Leap", "Sunlight Sensitivity", "Training",
-    "Treasure Sense", "Water Breathing", "Web Walker",
+    "Agile", "Amorphous", "Amphibious", "Beast of Burden", "Demonic Restoration", "Diabolical Restoration",
+    "Earth Glide", "False Appearance", "Flyby", "Hellish Restoration", "Hold Breath", "Ice Walk", "Illumination",
+    "Incorporeal Movement", "Jumper", "Keen Hearing", "Keen Hearing and Sight", "Keen Hearing and Smell", "Keen Sight",
+    "Keen Smell", "Limited Amphibiousness", "Mimicry", "Running Leap", "Shark Telepathy", "Spider Climb",
+    "Standing Leap", "Sunlight Sensitivity", "Sunlight Weakness", "Training", "Treasure Sense", "Water Breathing",
+    "Web Walker",
 })
 
 
