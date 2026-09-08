a8b6540781e2083889c24b0ef4aa9279a76d4383
Chris Wilson <divclass01@gmail.com>
2026-09-08 15:39:29 -0400

    treat siege monster as standard-pit neutral

--- backend/app/content/monster_trait_source_audit.py
+++ backend/app/content/monster_trait_source_audit.py
@@ -22,7 +22,7 @@ _ARENA_NEUTRAL_TRAITS = frozenset({
     "Agile", "Amorphous", "Amphibious", "Beast of Burden", "Demonic Restoration", "Diabolical Restoration",
     "Earth Glide", "False Appearance", "Flyby", "Hellish Restoration", "Hold Breath", "Ice Walk", "Illumination",
     "Incorporeal Movement", "Jumper", "Keen Hearing", "Keen Hearing and Sight", "Keen Hearing and Smell", "Keen Sight",
-    "Keen Smell", "Limited Amphibiousness", "Mimicry", "Running Leap", "Shark Telepathy", "Spider Climb",
+    "Keen Smell", "Limited Amphibiousness", "Mimicry", "Running Leap", "Shark Telepathy", "Siege Monster", "Spider Climb",
     "Standing Leap", "Sunlight Sensitivity", "Sunlight Weakness", "Training", "Treasure Sense", "Water Breathing",
     "Web Walker",
 })
