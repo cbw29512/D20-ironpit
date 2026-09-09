03071e3853a2996aa94e85f23f6611ca1e3b5359
Chris Wilson <divclass01@gmail.com>
2026-09-09 11:39:26 -0400

    cert: classify incorporeal movement as arena-neutral

--- backend/app/content/monster_trait_source_audit.py
+++ backend/app/content/monster_trait_source_audit.py
@@ -18,7 +18,7 @@ _MODELED_TRAITS = {
 }
 _ARENA_NEUTRAL_TRAITS = frozenset({
     "Agile", "Amphibious", "Beast of Burden", "Earth Glide", "False Appearance", "Flyby", "Hellish Restoration",
-    "Hold Breath", "Ice Walk", "Illumination", "Jumper", "Keen Hearing", "Keen Hearing and Sight",
+    "Hold Breath", "Ice Walk", "Illumination", "Incorporeal Movement", "Jumper", "Keen Hearing", "Keen Hearing and Sight",
     "Keen Hearing and Smell", "Keen Sight", "Keen Smell", "Limited Amphibiousness", "Mimicry",
     "Running Leap", "Spider Climb", "Standing Leap", "Sunlight Sensitivity", "Training", "Treasure Sense",
     "Water Breathing", "Web Walker",
