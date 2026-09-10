071ba1faf07d9db85118b2000c650eb57e7aaa44
Chris Wilson <divclass01@gmail.com>
2026-09-10 15:41:20 -0400

    browser: link timed restrictions to conditions

--- frontend/browser-timed-conditions.js
+++ frontend/browser-timed-conditions.js
@@ -32,6 +32,7 @@
       turn_behavior: options.turnBehavior || "normal",
       action_or_bonus_only: Boolean(options.actionOrBonusOnly),
       reactions_disabled: Boolean(options.reactionsDisabled),
+      requires_active_effect_id: options.requiresActiveEffectId || null,
       ends_on_damage: Boolean(options.endsOnDamage),
       ends_if_source_incapacitated: Boolean(options.endsIfSourceIncapacitated),
       ends_if_source_dead: Boolean(options.endsIfSourceDead),
