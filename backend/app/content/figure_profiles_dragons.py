e8a5d6924fa2b12258724032941b3d8af722d724
Chris Wilson <divclass01@gmail.com>
2026-09-09 23:09:45 -0400

    refactor: isolate dragon figure profiles

---
+++ backend/app/content/figure_profiles_dragons.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from app.content.figure_profile_schema import FigureProfile, figure_profile
+
+DRAGON_FIGURE_PROFILES: dict[str, FigureProfile] = {
+    "Black Dragon Wyrmling": figure_profile("reptile", "black-dragon-wyrmling"),
+    "Blue Dragon Wyrmling": figure_profile("reptile", "blue-dragon-wyrmling"),
+    "Green Dragon Wyrmling": figure_profile("reptile", "green-dragon-wyrmling"),
+    "Red Dragon Wyrmling": figure_profile("reptile", "red-dragon-wyrmling"),
+    "White Dragon Wyrmling": figure_profile("reptile", "white-dragon-wyrmling"),
+    "Young Black Dragon": figure_profile("reptile", "young-black-dragon"),
+    "Young Blue Dragon": figure_profile("reptile", "young-blue-dragon"),
+    "Young Green Dragon": figure_profile("reptile", "young-green-dragon"),
+    "Young Red Dragon": figure_profile("reptile", "young-red-dragon"),
+    "Young White Dragon": figure_profile("reptile", "young-white-dragon"),
+}
