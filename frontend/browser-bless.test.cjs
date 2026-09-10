b7a61a00d0076aae56eddd10ba9dd14378e23e30
Chris Wilson <divclass01@gmail.com>
2026-09-10 18:38:15 -0400

    browser: load timed effects in Bless parity harness

--- frontend/browser-bless.test.cjs
+++ frontend/browser-bless.test.cjs
@@ -10,7 +10,7 @@ const load = (name) => vm.runInThisContext(fs.readFileSync(path.join(__dirname, 
 for (const file of [
   "browser-heroes.js", "browser-condition-immunity.js", "browser-condition-rules.js", "browser-action-economy.js",
   "browser-grapple.js", "browser-modifiers.js", "browser-state.js", "browser-rage.js", "browser-rolls.js",
-  "browser-undead-fortitude.js", "browser-zero-hp.js", "browser-attack.js", "browser-saves.js",
+  "browser-timed-conditions.js", "browser-undead-fortitude.js", "browser-zero-hp.js", "browser-attack.js", "browser-saves.js",
   "browser-concentration.js", "browser-spell-effects.js", "browser-spell-modifiers.js", "browser-precombat-spells.js",
 ]) load(file);
 
