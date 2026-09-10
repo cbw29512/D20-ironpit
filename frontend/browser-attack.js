80ecab51a5939c603ac2a066ee123b6b00050c68
Chris Wilson <divclass01@gmail.com>
2026-09-09 21:44:54 -0400

    fix: restore browser attack source limit

--- frontend/browser-attack.js
+++ frontend/browser-attack.js
@@ -13,8 +13,7 @@
     attacksAgainstAdvantage: () => 0, consumeAttacksAgainstAdvantage: () => 0, nextAttackAgainstAdvantage: () => 0,
     consumeNextAttackAgainstAdvantage: () => 0, effectiveArmorClass: (state) => state.template.armor_class,
     effectiveSpeed: (state) => state.template.speed_ft, applyD20Bonus: (_state, _kind, roll) => roll,
-  };
-  const C = () => window.IRON_PIT_BROWSER_CONCENTRATION, I = () => window.IRON_PIT_BROWSER_CONDITION_IMMUNITY || { immune: () => false }, F = () => window.IRON_PIT_BROWSER_FRIGHTENED;
+  }; const C = () => window.IRON_PIT_BROWSER_CONCENTRATION, I = () => window.IRON_PIT_BROWSER_CONDITION_IMMUNITY || { immune: () => false }, F = () => window.IRON_PIT_BROWSER_FRIGHTENED;
   const Q = () => window.IRON_PIT_BROWSER_CONDITION_RULES || {
     attackAdvantage: (state) => state.is_unconscious, autoCritical: (state) => state.is_unconscious,
     has: (state, id) => state.active_effect_ids.includes(id), incapacitated: (state) => state.is_unconscious,
