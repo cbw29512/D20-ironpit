0420e2486bb2d4083deeb8570934fbe9a893894d
Chris Wilson <divclass01@gmail.com>
2026-09-11 00:19:15 -0400

    audit: mark off-arena restoration traits combat-neutral

--- backend/app/content/monster_trait_source_audit.py
+++ backend/app/content/monster_trait_source_audit.py
@@ -22,7 +22,7 @@ _MODELED_TRAITS = {
 }
 _DECLARATIVE_ATTACK_TRAITS = frozenset({"Blood Frenzy"})
 _ARENA_NEUTRAL_TRAITS = frozenset({
-    "Agile", "Amorphous", "Amphibious", "Beast of Burden", "Demonic Restoration", "Earth Glide", "False Appearance", "Flyby",
+    "Agile", "Amorphous", "Amphibious", "Beast of Burden", "Demonic Restoration", "Diabolical Restoration", "Earth Glide", "Exalted Restoration", "False Appearance", "Flyby",
     "Hellish Restoration", "Hold Breath", "Ice Walk", "Illumination", "Jumper", "Keen Hearing",
     "Keen Hearing and Sight", "Keen Hearing and Smell", "Keen Sight", "Keen Smell", "Limited Amphibiousness",
     "Mimicry", "Running Leap", "Shark Telepathy", "Siege Monster", "Spider Climb", "Standing Leap", "Sunlight Sensitivity", "Sunlight Weakness",
