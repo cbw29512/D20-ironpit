3c6ff9706a574976d92a69cbcb1bb6e21ff72b17
Chris Wilson <divclass01@gmail.com>
2026-09-12 06:37:20 -0400

    test: load resources in spell-slot healing harness

--- frontend/browser-spell-slot-healing.test.cjs
+++ frontend/browser-spell-slot-healing.test.cjs
@@ -9,7 +9,7 @@ global.window = globalThis;
 const load = (name) => vm.runInThisContext(fs.readFileSync(path.join(__dirname, name), "utf8"), { filename: name });
 for (const file of [
   "browser-heroes.js", "browser-condition-rules.js", "browser-action-economy.js", "browser-modifiers.js",
-  "browser-state.js", "browser-spellcasting.js", "browser-healing.js", "browser-spell-area.js",
+  "browser-state.js", "browser-resources.js", "browser-spellcasting.js", "browser-healing.js", "browser-spell-area.js",
   "browser-rolls.js", "browser-attack.js", "browser-saves.js", "browser-offense-value.js",
   "browser-spell-policy.js", "browser-spell-attack-policy.js",
 ]) load(file);
