cc28bd23a64e632cfb01c3d49b26f9b5ae2873aa
Chris Wilson <divclass01@gmail.com>
2026-09-11 21:53:17 -0400

    Add Magic Weapons combat trait

--- backend/app/domain/traits.py
+++ backend/app/domain/traits.py
@@ -11,4 +11,5 @@ class CombatTrait(StrEnum):
     SWARM = "swarm"
     UNDEAD_FORTITUDE = "undead-fortitude"
     MAGIC_RESISTANCE = "magic-resistance"
+    MAGIC_WEAPONS = "magic-weapons"
     LIFE_DOMAIN = "life-domain"
