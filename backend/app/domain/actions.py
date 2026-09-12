243b8d853fc0c18d2beaa76c8b325583837802f6
Chris Wilson <divclass01@gmail.com>
2026-09-11 21:50:50 -0400

    Mark save actions that originate from magical effects

--- backend/app/domain/actions.py
+++ backend/app/domain/actions.py
@@ -116,6 +116,7 @@ class SavingThrowAction(BaseModel):
     restrains_while_grappled: bool = False
     resource_id: str | None = None
     resource_cost: int = Field(default=1, ge=1, le=20)
+    magical_effect: bool = False
     animation: str = "save-effect"
 
 
