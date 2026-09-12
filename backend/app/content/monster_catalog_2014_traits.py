a7a86d66cd70160a8f0dea807a0fb46a2b7650a7
Chris Wilson <divclass01@gmail.com>
2026-09-12 10:32:46 -0400

    Classify Iron Pit neutral environmental traits

--- backend/app/content/monster_catalog_2014_traits.py
+++ backend/app/content/monster_catalog_2014_traits.py
@@ -25,8 +25,9 @@ ARENA_NEUTRAL_TRAITS = frozenset({
     "Hellish Restoration", "Hold Breath", "Ice Walk", "Illumination", "Jumper",
     "Keen Hearing", "Keen Hearing and Sight", "Keen Hearing and Smell", "Keen Sight",
     "Keen Sight and Smell", "Keen Smell", "Limited Amphibiousness", "Mimicry", "Rejuvenation",
-    "Running Leap", "Spider Climb", "Standing Leap", "Stone Camouflage", "Sunlight Sensitivity",
-    "Training", "Water Breathing", "Web Walker",
+    "Running Leap", "Shark Telepathy", "Snow Camouflage", "Spider Climb", "Standing Leap",
+    "Stone Camouflage", "Sunlight Sensitivity", "Training", "Water Breathing", "Web Sense",
+    "Web Walker",
 })
 
 SUPPORTED_TRAITS = frozenset(MODELED_TRAITS) | DATA_BOUND_TRAITS | ARENA_NEUTRAL_TRAITS
