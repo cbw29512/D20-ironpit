803a44b80091cb328f329c84f44af3303477bd7b
Chris Wilson <divclass01@gmail.com>
2026-09-11 20:16:34 -0400

    content: add Brass dragon reviewed figure profiles

--- backend/app/content/figure_profiles_dragons.py
+++ backend/app/content/figure_profiles_dragons.py
@@ -5,6 +5,7 @@ from app.content.figure_profile_schema import FigureProfile, figure_profile
 DRAGON_FIGURE_PROFILES: dict[str, FigureProfile] = {
     "Black Dragon Wyrmling": figure_profile("reptile", "black-dragon-wyrmling"),
     "Blue Dragon Wyrmling": figure_profile("reptile", "blue-dragon-wyrmling"),
+    "Brass Dragon Wyrmling": figure_profile("reptile", "brass-dragon-wyrmling"),
     "Bronze Dragon Wyrmling": figure_profile("reptile", "bronze-dragon-wyrmling"),
     "Dragon Turtle": figure_profile("aquatic-reptile", "dragon-turtle"),
     "Green Dragon Wyrmling": figure_profile("reptile", "green-dragon-wyrmling"),
@@ -13,6 +14,7 @@ DRAGON_FIGURE_PROFILES: dict[str, FigureProfile] = {
     "White Dragon Wyrmling": figure_profile("reptile", "white-dragon-wyrmling"),
     "Young Black Dragon": figure_profile("reptile", "young-black-dragon"),
     "Young Blue Dragon": figure_profile("reptile", "young-blue-dragon"),
+    "Young Brass Dragon": figure_profile("reptile", "young-brass-dragon"),
     "Young Bronze Dragon": figure_profile("reptile", "young-bronze-dragon"),
     "Young Copper Dragon": figure_profile("reptile", "young-copper-dragon"),
     "Young Green Dragon": figure_profile("reptile", "young-green-dragon"),
