607df7d54ae2612569d83cbf66e414ba8cf70381
Chris Wilson <divclass01@gmail.com>
2026-09-13 02:32:04 -0400

    Allow isolated charge tests without death triggers

--- frontend/browser-charge.js
+++ frontend/browser-charge.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const A = () => window.IRON_PIT_BROWSER_ATTACK;
-  const DT = () => window.IRON_PIT_BROWSER_DEATH_TRIGGERS;
+  const DT = () => window.IRON_PIT_BROWSER_DEATH_TRIGGERS || { resolvePending: (sequence) => ({ events: [], sequence }) };
   const E = () => window.IRON_PIT_ACTION_ECONOMY || { available: (s) => s.action_available };
 
   function openingEligible(round, member, setup) {
