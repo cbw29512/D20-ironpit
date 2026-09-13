d0d7079445c52a2f3e104f6648a34c6096b5d52c
Chris Wilson <divclass01@gmail.com>
2026-09-13 10:01:02 -0400

    2014: mark Barbed Hide as data-bound

--- backend/app/content/monster_catalog_2014_traits.py
+++ backend/app/content/monster_catalog_2014_traits.py
@@ -22,7 +22,7 @@ MODELED_TRAITS = {
 }
 
 DATA_BOUND_TRAITS = frozenset({
-    "Angelic Weapons", "Blood Frenzy", "Echolocation", "Innate Spellcasting",
+    "Angelic Weapons", "Barbed Hide", "Blood Frenzy", "Echolocation", "Innate Spellcasting",
     "Petrifying Gaze", "Poor Depth Perception", "Reckless", "Regeneration", "Spellcasting", "Stench",
     "Turning Defiance",
 })
