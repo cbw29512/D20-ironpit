ae95a4137889e8eb3a870d96ca66c6ca7c41f487
Chris Wilson <divclass01@gmail.com>
2026-09-14 11:31:58 -0400

    engine: bind Aversion of Fire damage trigger

--- backend/app/content/monster_catalog_2014_traits.py
+++ backend/app/content/monster_catalog_2014_traits.py
@@ -31,7 +31,7 @@ MODELED_TRAITS = {
 }
 
 DATA_BOUND_TRAITS = frozenset({
-    "Angelic Weapons", "Barbed Hide", "Blood Frenzy", "Brute", "Echolocation", "Fear of Fire",
+    "Angelic Weapons", "Aversion of Fire", "Barbed Hide", "Blood Frenzy", "Brute", "Echolocation", "Fear of Fire",
     "Heated Body", "Heated Weapons", "Innate Spellcasting", "Invisibility", "Petrifying Gaze", "Poor Depth Perception",
     "Reckless", "Regeneration", "Spellcasting", "Stench", "Turning Defiance",
 }) | ABSORPTION_TRAITS_2014
