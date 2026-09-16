70089bc7f82eafdb023c2ea313abe6ebc8bd0bfc
Chris Wilson <divclass01@gmail.com>
2026-09-16 11:16:19 -0400

    test: run expanded 73-monster 2014 browser lane

--- frontend/browser-2014-mvp.test.cjs
+++ frontend/browser-2014-mvp.test.cjs
@@ -12,13 +12,13 @@ const deterministicDice = (seed = 2014) => {
 };
 
 assert.equal(window.IRON_PIT_2014_MVP_READY, true, "2014 browser test roster must be loaded");
-assert.equal(Object.keys(window.IRON_PIT_BROWSER_MONSTERS_2014).length, 67);
+assert.equal(Object.keys(window.IRON_PIT_BROWSER_MONSTERS_2014).length, 73);
 
 window.IRON_PIT_DICE = deterministicDice();
 const battle = window.IRON_PIT_BROWSER_ENGINE.runEncounter({
   ruleset: "2014",
-  hero_ids: ["2014-brown-bear", "2014-bandit"],
-  monster_ids: ["2014-skeleton", "2014-goblin"],
+  hero_ids: ["2014-dire-wolf", "2014-bandit"],
+  monster_ids: ["2014-wolf", "2014-goblin"],
 });
 
 assert.equal(battle.ruleset, "2014");
