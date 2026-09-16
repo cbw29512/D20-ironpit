84d7a56d642c83c8865065285dab799ce85c449e
Chris Wilson <divclass01@gmail.com>
2026-09-16 14:41:38 -0400

    test: certify Mule in isolated 2014 data lane

--- frontend/browser-ruleset-data.test.cjs
+++ frontend/browser-ruleset-data.test.cjs
@@ -25,13 +25,13 @@ assertRuleset(Object.values(window.IRON_PIT_BROWSER_MONSTERS), "2024", "legacy b
 
 load("browser-monsters-2014.js");
 const monsters2014 = Object.values(window.IRON_PIT_BROWSER_MONSTERS_2014);
-assert.equal(monsters2014.length, 91, "2014 browser roster must contain exactly 91 certified monsters");
+assert.equal(monsters2014.length, 92, "2014 browser roster must contain exactly 92 certified monsters");
 for (const id of [
   "2014-bandit", "2014-brown-bear", "2014-goblin", "2014-skeleton", "2014-fire-giant", "2014-owlbear",
   "2014-badger", "2014-cat", "2014-crab", "2014-hawk", "2014-lizard", "2014-rat", "2014-weasel",
   "2014-baboon", "2014-blood-hawk", "2014-giant-rat", "2014-ogre-zombie", "2014-raven", "2014-zombie",
   "2014-awakened-shrub", "2014-awakened-tree", "2014-giant-fire-beetle", "2014-giant-owl",
-  "2014-kobold", "2014-owl", "2014-pteranodon", "2014-twig-blight",
+  "2014-kobold", "2014-mule", "2014-owl", "2014-pteranodon", "2014-twig-blight",
   "2014-constrictor-snake", "2014-crocodile", "2014-flying-snake", "2014-giant-constrictor-snake",
   "2014-giant-crab", "2014-roc", "2014-tyrannosaurus-rex",
   "2014-ankylosaurus", "2014-dire-wolf", "2014-giant-crocodile", "2014-mastiff", "2014-wolf", "2014-worg",
@@ -52,6 +52,9 @@ for (const id of [
   assert.equal(swarm.attacks[0].conditionalDamage.trigger, "attacker_bloodied");
   assert.equal(swarm.attacks[0].conditionalDamage.mode, "replace_weapon");
 }
+const mule = monsters2014.find((monster) => monster.id === "2014-mule");
+assert.ok(mule, "2014-mule must exist before trait assertions");
+assert.deepEqual(mule.traits, ["sure-footed"], "Beast of Burden stays arena-neutral while Sure-Footed remains modeled");
 assertRuleset(monsters2014, "2014", "2014 browser monsters");
 assert.equal(window.IRON_PIT_2014_MVP_READY, true);
 console.log("Browser combatants carry explicit isolated ruleset identity for 2014 and 2024.");
