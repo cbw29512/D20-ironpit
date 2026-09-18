12586919fc08c63ad8fe7031d36fab4664d19ee7
Chris Wilson <divclass01@gmail.com>
2026-09-18 18:17:04 -0400

    Install post-action Bonus Action hooks

--- frontend/browser-ability-hook-installation.js
+++ frontend/browser-ability-hook-installation.js
@@ -8,6 +8,8 @@
     const installers = [
       ["Rage", window.IRON_PIT_BROWSER_RAGE?.installAbilityHooks],
       ["Support", window.IRON_PIT_BROWSER_SUPPORT?.installAbilityHooks],
+      ["2014 Monk", window.IRON_PIT_BROWSER_MONK_2014?.installAbilityHooks],
+      ["2014 Frenzy", window.IRON_PIT_BROWSER_FRENZY_2014?.installAbilityHooks],
     ];
     for (const [name, installer] of installers) {
       if (typeof installer !== "function") throw new Error(`${name} ability hook installer is not loaded.`);
