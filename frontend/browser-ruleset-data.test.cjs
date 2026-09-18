3778bcaada7a2a2fca731f7e61e2ad67b6693962
Chris Wilson <divclass01@gmail.com>
2026-09-18 04:49:28 -0400

    Advance browser-ruleset-data.test.cjs to 112 certified 2014 monsters

--- frontend/browser-ruleset-data.test.cjs
+++ frontend/browser-ruleset-data.test.cjs
@@ -77,7 +77,7 @@ assertRuleset(Object.values(window.IRON_PIT_BROWSER_MONSTERS), "2024", "legacy b
 
 load("browser-monsters-2014.js");
 const monsters2014 = Object.values(window.IRON_PIT_BROWSER_MONSTERS_2014);
-assert.equal(monsters2014.length, 101, "2014 browser roster must contain exactly 101 certified monsters");
+assert.equal(monsters2014.length, 112, "2014 browser roster must contain exactly 101 certified monsters");
 for (const id of [
   "2014-bandit", "2014-brown-bear", "2014-goblin", "2014-skeleton", "2014-fire-giant", "2014-owlbear",
   "2014-badger", "2014-cat", "2014-crab", "2014-hawk", "2014-lizard", "2014-rat", "2014-weasel",
