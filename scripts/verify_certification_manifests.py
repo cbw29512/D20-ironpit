1d7c38f4ee97ebb19eb1441387fc336047a09d93
Chris Wilson <divclass01@gmail.com>
2026-09-18 12:46:42 -0400

    Track Brutal Strike in certification manifests

--- scripts/verify_certification_manifests.py
+++ scripts/verify_certification_manifests.py
@@ -56,6 +56,8 @@ def _mechanics(template: Any) -> list[str]:
         mechanics.add("frenzy")
     if features.frenzy_bonus_attack_2014:
         mechanics.add("frenzy-bonus-attack-2014")
+    if features.brutal_strike_damage_dice:
+        mechanics.add("brutal-strike")
     if features.fast_movement_bonus_ft:
         mechanics.add("fast-movement")
     if features.mindless_rage:
