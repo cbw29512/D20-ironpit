c235f2c628ce15b72ce7db2cc493869bbcc8eaf3
Chris Wilson <divclass01@gmail.com>
2026-09-18 09:16:45 -0400

    Restore universal Aggressive and Rampage traits

--- backend/app/domain/traits.py
+++ backend/app/domain/traits.py
@@ -4,6 +4,8 @@ from enum import StrEnum
 class CombatTrait(StrEnum):
     PACK_TACTICS = "pack-tactics"
     RECKLESS = "reckless"
+    AGGRESSIVE = "aggressive"
+    RAMPAGE = "rampage"
     SAVAGE_ATTACKER = "savage-attacker"
     ADRENALINE_RUSH = "adrenaline-rush"
     RELENTLESS_ENDURANCE = "relentless-endurance"
