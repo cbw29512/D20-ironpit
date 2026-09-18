df81bc96ec033c24b2c506e3fcf132c2af5ba28c
Chris Wilson <divclass01@gmail.com>
2026-09-18 09:06:24 -0400

    Split 2014 arena-neutral trait policy

---
+++ backend/app/content/monster_arena_neutral_traits_2014.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+ARENA_NEUTRAL_TRAITS_2014 = frozenset({
+    "Amphibious", "Beast of Burden", "Echolocation", "False Appearance", "Flyby", "Hold Breath",
+    "Ice Walk", "Illumination", "Keen Hearing", "Keen Hearing and Smell", "Keen Hearing and Sight",
+    "Keen Sight", "Keen Sight and Smell", "Keen Smell", "Labyrinthine Recall", "Mimicry",
+    "Running Leap", "Snow Camouflage", "Spider Climb", "Sunlight Sensitivity", "Water Breathing",
+    "Web Sense", "Web Walker",
+})
