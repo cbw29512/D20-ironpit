dbcc44b4c9adc89fd83251c2001d01d6a20f10cd
Chris Wilson <divclass01@gmail.com>
2026-09-18 21:00:23 -0400

    Load attack outcome hooks in browser-studied-attacks.test.cjs

--- frontend/browser-studied-attacks.test.cjs
+++ frontend/browser-studied-attacks.test.cjs
@@ -14,10 +14,10 @@ for (const htmlPath of [path.join(__dirname, "index.html"), path.join(__dirname,
   assert.ok(html.indexOf("browser-studied-attacks.js") < html.indexOf("browser-attack.js"));
 }
 for (const file of [
-  "browser-heroes.js", "browser-condition-immunity.js", "browser-condition-rules.js", "browser-action-economy.js",
+  "browser-heroes.js", "browser-condition-immunity.js", "browser-condition-rules.js", "browser-action-economy.js", "browser-ability-hooks.js",
   "browser-grapple.js", "browser-modifiers.js", "browser-weapon-mastery.js", "browser-graze.js",
   "browser-studied-attacks.js", "browser-heroic-inspiration.js", "browser-state.js", "browser-rage.js",
-  "browser-rolls.js", "browser-undead-fortitude.js", "browser-zero-hp.js", "browser-attack.js",
+  "browser-rolls.js", "browser-undead-fortitude.js", "browser-zero-hp.js", "browser-attack.js", "browser-attack-outcome-hooks.js",
 ]) load(file);
 
 const S = window.IRON_PIT_BROWSER_STATE, M = window.IRON_PIT_BROWSER_MODIFIERS;
