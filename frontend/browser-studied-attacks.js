c3777a23e8e4c242d7bd3f6f7a2a3ad2caf7422e
Chris Wilson <divclass01@gmail.com>
2026-09-18 21:41:59 -0400

    Queue browser-studied-attacks.js hook installer when dispatcher loads late

--- frontend/browser-studied-attacks.js
+++ frontend/browser-studied-attacks.js
@@ -44,4 +44,5 @@
 
   window.IRON_PIT_BROWSER_STUDIED_ATTACKS = { active, apply, installAbilityHooks, resolveMiss };
   if (window.IRON_PIT_BROWSER_ABILITY_HOOKS) installAbilityHooks();
+  else (window.IRON_PIT_PENDING_ABILITY_HOOK_INSTALLERS ||= []).push(installAbilityHooks);
 })();
