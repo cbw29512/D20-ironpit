684743857fbd682fe3a4967c886b2c25c7280298
Chris Wilson <divclass01@gmail.com>
2026-09-18 23:22:48 -0400

    Require explicit Main Action provider load order

--- frontend/browser-main-action-spell-provider.js
+++ frontend/browser-main-action-spell-provider.js
@@ -21,6 +21,5 @@
   }
 
   window.IRON_PIT_BROWSER_MAIN_ACTION_SPELL_PROVIDER = { install };
-  if (window.IRON_PIT_BROWSER_MAIN_ACTION_SELECTION) install();
-  else (window.IRON_PIT_PENDING_MAIN_ACTION_PROVIDER_INSTALLERS ||= []).push(install);
+  install();
 })();
