b950d4da113a32d11fffd33d73f9110f32f5db3d
Chris Wilson <divclass01@gmail.com>
2026-09-21 15:52:17 -0400

    Add universal timed and deferred action categories

--- frontend/browser-main-action-profiles.js
+++ frontend/browser-main-action-profiles.js
@@ -2,8 +2,10 @@
   "use strict";
 
   const CATEGORIES = Object.freeze({
+    TIMED_SELF_BUFF: "timed-self-buff",
     SPELL_OFFENSE: "spell-offense",
     INTIMIDATING_PRESENCE_2014: "intimidating-presence-2014",
+    DEFERRED_EFFECT: "deferred-effect",
     ATTACK_ACTION: "attack-action",
     AREA_SAVE: "area-save",
     SAVE_ACTION: "save-action",
@@ -12,10 +14,11 @@
   });
 
   const PROFILES = Object.freeze({
-    normalPreMove: Object.freeze([CATEGORIES.SPELL_OFFENSE]),
+    normalPreMove: Object.freeze([CATEGORIES.TIMED_SELF_BUFF, CATEGORIES.SPELL_OFFENSE]),
     normalPostMove: Object.freeze([
       CATEGORIES.SPELL_OFFENSE,
       CATEGORIES.INTIMIDATING_PRESENCE_2014,
+      CATEGORIES.DEFERRED_EFFECT,
       CATEGORIES.ATTACK_ACTION,
       CATEGORIES.AREA_SAVE,
       CATEGORIES.SAVE_ACTION,
