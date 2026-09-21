3b5eba350ff06e62eb7f95ddf57a1713ccd87723
Chris Wilson <divclass01@gmail.com>
2026-09-21 09:58:02 -0400

    Fix browser feature-name capitalization regex

--- frontend/browser-attack.js
+++ frontend/browser-attack.js
@@ -142,7 +142,7 @@
     const attackSave = saveDamage?.saveDc != null ? saveDamage : hitSave;
     const survivalLog = window.IRON_PIT_BROWSER_UNDEAD_FORTITUDE?.consumeLog(actualTarget.state) || "";
     let description = `${attacker.state.template.name}: ${critical ? "CRITICAL HIT" : hit ? "HIT" : "MISS"} with ${attack.name}.`;
-    if (override.featureId) description += ` ${override.featureId.replaceAll("-", " ").replace(/\\b\\w/g, (char) => char.toUpperCase())} turns the miss into a hit.`;
+    if (override.featureId) description += ` ${override.featureId.replaceAll("-", " ").replace(/\b\w/g, (char) => char.toUpperCase())} turns the miss into a hit.`;
     else if (naturalOneEndsTurn) description += " Natural 1: Iron Pit immediately ends the attacker's turn.";
     else if (naturalOne) description += " Natural 1: automatic miss; this off-turn attack does not terminate a future turn.";
     if (heroic.used) description += " Heroic Inspiration rerolls one d20.";
