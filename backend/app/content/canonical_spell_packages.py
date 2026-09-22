28dab20ad178be41b3736a999fff649ea77957b1
Chris Wilson <divclass01@gmail.com>
2026-09-21 21:12:12 -0400

    Add simple-policy Cleric level 7-8 spell package

--- backend/app/content/canonical_spell_packages.py
+++ backend/app/content/canonical_spell_packages.py
@@ -93,6 +93,22 @@ CANONICAL_SPELLS: dict[CasterClassId, tuple[CanonicalSpellChoice, ...]] = {
             "revivify", "Revivify", 3, "healing", 5,
             "arena-out-of-scope", always_prepared_from_level=5,
         ),
+        _later_spell(
+            "aura-of-life", "Aura of Life", 4, "healing", 7,
+            "arena-out-of-scope", always_prepared_from_level=7,
+        ),
+        _later_spell(
+            "death-ward", "Death Ward", 4, "healing", 7,
+            "arena-out-of-scope", always_prepared_from_level=7,
+        ),
+        _later_spell(
+            "prayer-of-healing", "Prayer of Healing", 2, "healing", 7,
+            "arena-out-of-scope",
+        ),
+        _later_spell(
+            "guardian-of-faith", "Guardian of Faith", 4, "damage", 8,
+            "arena-out-of-scope",
+        ),
     ),
     "druid": (
         _spell("animal-friendship", "Animal Friendship", "control", "charmed"),
