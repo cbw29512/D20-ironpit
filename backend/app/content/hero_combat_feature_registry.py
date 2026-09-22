eec042214ff8feb6d387f073a8f3b3ff40274ae4
Chris Wilson <divclass01@gmail.com>
2026-09-22 16:47:50 -0400

    Bind Boon of Combat Prowess to universal miss-to-hit lifecycle

--- backend/app/content/hero_combat_feature_registry.py
+++ backend/app/content/hero_combat_feature_registry.py
@@ -13,7 +13,7 @@ SUPPORTED_HERO_ENGINE_FEATURES = {
     "survivor-defy-death", "survivor-heroic-rally",
     "rage", "danger-sense", "reckless-attack", "frenzy", "fast-movement", "mindless-rage",
     "relentless-rage", "feral-instinct", "instinctive-pounce", "brutal-strike", "brutal-strike-2d10",
-    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim", "cunning-strike", "improved-cunning-strike", "devious-strikes", "slippery-mind", "thiefs-reflexes", "elusive", "uncanny-dodge", "evasion",
+    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim", "cunning-strike", "improved-cunning-strike", "devious-strikes", "slippery-mind", "thiefs-reflexes", "elusive", "boon-combat-prowess", "uncanny-dodge", "evasion",
     "cleric-spellcasting", "divine-order-protector", "divine-spark", "turn-undead",
     "disciple-of-life", "preserve-life", "sear-undead", "cleric-combat-spells-3", "blessed-healer",
     "blessed-strikes", "cleric-combat-spells-5", "divine-intervention", "cleric-combat-spells-6",
@@ -56,6 +56,11 @@ _STATIC_PROGRESSION_FIELDS: dict[str, dict[str, object]] = {
         ],
     },
     "elusive": {"suppress_attack_advantage_while_not_incapacitated": True},
+    "boon-combat-prowess": {
+        "miss_to_hit_override_resource_id": "boon-combat-prowess",
+        "miss_to_hit_override_source_name": "Boon of Combat Prowess",
+        "start_of_turn_resource_refresh_ids": ["boon-combat-prowess"],
+    },
     "uncanny-dodge": {"uncanny_dodge": True},
     "evasion": {"evasion": True},
 }
