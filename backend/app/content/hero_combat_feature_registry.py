c381490840e0f359cd540e882e34b8c2709e08cd
Chris Wilson <divclass01@gmail.com>
2026-09-22 09:08:05 -0400

    Admit Improved Cunning Strike to shared Rogue feature registry

--- backend/app/content/hero_combat_feature_registry.py
+++ backend/app/content/hero_combat_feature_registry.py
@@ -13,7 +13,7 @@ SUPPORTED_HERO_ENGINE_FEATURES = {
     "survivor-defy-death", "survivor-heroic-rally",
     "rage", "danger-sense", "reckless-attack", "frenzy", "fast-movement", "mindless-rage",
     "relentless-rage", "feral-instinct", "instinctive-pounce", "brutal-strike", "brutal-strike-2d10",
-    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim", "cunning-strike", "uncanny-dodge", "evasion",
+    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim", "cunning-strike", "improved-cunning-strike", "uncanny-dodge", "evasion",
     "cleric-spellcasting", "divine-order-protector", "divine-spark", "turn-undead",
     "disciple-of-life", "preserve-life", "sear-undead", "cleric-combat-spells-3", "blessed-healer",
     "blessed-strikes", "cleric-combat-spells-5", "divine-intervention", "cleric-combat-spells-6",
