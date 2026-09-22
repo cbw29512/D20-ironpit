d3bb0a25b35f412683369cfc437dc7e4f04844e7
Chris Wilson <divclass01@gmail.com>
2026-09-22 08:01:15 -0400

    Reuse universal Uncanny Dodge for 2024 Rogue 5

--- backend/app/content/hero_combat_feature_registry.py
+++ backend/app/content/hero_combat_feature_registry.py
@@ -13,7 +13,7 @@ SUPPORTED_HERO_ENGINE_FEATURES = {
     "survivor-defy-death", "survivor-heroic-rally",
     "rage", "danger-sense", "reckless-attack", "frenzy", "fast-movement", "mindless-rage",
     "relentless-rage", "feral-instinct", "instinctive-pounce", "brutal-strike", "brutal-strike-2d10",
-    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim",
+    "sneak-attack", "weapon-mastery", "cunning-action", "steady-aim", "uncanny-dodge",
     "cleric-spellcasting", "divine-order-protector", "divine-spark", "turn-undead",
     "disciple-of-life", "preserve-life", "sear-undead", "cleric-combat-spells-3", "blessed-healer",
     "blessed-strikes", "cleric-combat-spells-5", "divine-intervention", "cleric-combat-spells-6",
@@ -42,6 +42,7 @@ _STATIC_PROGRESSION_FIELDS: dict[str, dict[str, object]] = {
     "brutal-strike": {"brutal_strike_damage_dice": 1},
     "cunning-action": {"cunning_action": True},
     "steady-aim": {"stationary_bonus_action_next_attack_advantage": True},
+    "uncanny-dodge": {"uncanny_dodge": True},
 }
 
 
