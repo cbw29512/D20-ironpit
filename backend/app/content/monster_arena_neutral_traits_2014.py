2ee28f9755f9a3b962d5ba88bb1d52487e79845c
Chris Wilson <divclass01@gmail.com>
2026-09-23 12:32:38 -0400

    Stop treating 2014 Flyby as arena neutral

--- backend/app/content/monster_arena_neutral_traits_2014.py
+++ backend/app/content/monster_arena_neutral_traits_2014.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 ARENA_NEUTRAL_TRAITS_2014 = frozenset({
-    "Amphibious", "Beast of Burden", "Echolocation", "False Appearance", "Flyby", "Hold Breath",
+    "Amphibious", "Beast of Burden", "Echolocation", "False Appearance", "Hold Breath",
     "Ice Walk", "Illumination", "Keen Hearing", "Keen Hearing and Smell", "Keen Hearing and Sight",
     "Keen Sight", "Keen Sight and Smell", "Keen Smell", "Labyrinthine Recall", "Mimicry",
     "Running Leap", "Snow Camouflage", "Spider Climb", "Sunlight Sensitivity", "Water Breathing",
