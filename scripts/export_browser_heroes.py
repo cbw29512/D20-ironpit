a939d7f5e4970b604283698b86726ffafb6f518d
Chris Wilson <divclass01@gmail.com>
2026-09-24 09:58:09 -0400

    feat: export split save spell damage components

--- scripts/export_browser_heroes.py
+++ scripts/export_browser_heroes.py
@@ -82,6 +82,13 @@ def _spell(action: Any) -> dict[str, Any]:
         "range": action.range_ft, "saveAbility": action.save_ability, "dc": action.dc,
         "damageDiceCount": action.damage_dice_count, "damageDiceSize": action.damage_dice_size,
         "damageBonus": action.damage_bonus, "damageType": action.damage_type,
+        "damageComponents": [{
+            "source": component.source,
+            "diceCount": component.dice_count,
+            "diceSize": component.dice_size,
+            "damageBonus": component.damage_bonus,
+            "damageType": component.damage_type,
+        } for component in action.damage_components],
         "successDamage": action.success_damage, "upcastDicePerLevel": action.upcast_dice_per_level,
         "concentration": action.concentration, "animation": action.animation,
     }
