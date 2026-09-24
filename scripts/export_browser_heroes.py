3364b619014361c3ca070ee4839fc88861cbed65
Chris Wilson <divclass01@gmail.com>
2026-09-23 22:16:48 -0400

    feat: export percentile full-heal parameters

--- scripts/export_browser_heroes.py
+++ scripts/export_browser_heroes.py
@@ -176,7 +176,8 @@ def _healing(action: Any) -> dict[str, Any]:
     return {"id": action.id, "name": action.name, "actionCost": action.action_cost, "range": action.range_ft,
             "targetMode": action.target_mode, "maxTargets": action.max_targets, "areaRadiusFt": action.area_radius_ft,
             "diceCount": action.dice_count, "diceSize": action.dice_size,
-            "healingBonus": action.healing_bonus, "resourceId": action.resource_id,
+            "healingBonus": action.healing_bonus, "restoreToEffectiveMax": action.restore_to_effective_max,
+            "percentileSuccessMax": action.percentile_success_max, "resourceId": action.resource_id,
             "resourceCost": action.resource_cost, "excludedCreatureTypes": list(action.excluded_creature_types),
             "animation": action.animation}
 
