71ab0c7a57b1ec89d31df61178b01d4ba9ef42d8
Chris Wilson <divclass01@gmail.com>
2026-09-24 21:12:59 -0400

    Register aura radius as progression parameter

--- scripts/export_runtime_monster_capabilities.py
+++ scripts/export_runtime_monster_capabilities.py
@@ -17,7 +17,7 @@ _HERO_ONLY_PROGRESSION_FIELDS = {
     "sneak_attack_d6", "uncanny_dodge", "evasion",
     "martial_arts_bonus_attack", "martial_arts_die_size", "flurry_of_blows", "deflect_missiles",
     "open_hand_technique", "stunning_strike", "divine_smite_2014", "turn_unholy_2014",
-    "aura_of_protection_2014_bonus", "aura_of_devotion_2014", "aura_of_courage_2014",
+    "aura_of_protection_2014_bonus", "aura_radius_2014_ft", "aura_of_devotion_2014", "aura_of_courage_2014",
     "sacred_weapon_2014_bonus", "survivor_heal_amount",
 }
 
