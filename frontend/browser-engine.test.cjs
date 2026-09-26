fdcb6860917091bd985588095a313444904cfb33
Chris Wilson <divclass01@gmail.com>
2026-09-26 14:59:45 -0400

    test: load resource conversion in browser engine

--- frontend/browser-engine.test.cjs
+++ frontend/browser-engine.test.cjs
@@ -12,7 +12,7 @@ for (const file of [
   "browser-heroes.js", "browser-monsters.js", "browser-monsters-fixed.js",
   "browser-condition-immunity.js", "browser-condition-rules.js", "browser-action-economy.js", "browser-ability-hooks.js",
   "browser-grapple.js", "browser-timed-conditions.js", "browser-weapon-mastery.js",
-  "browser-state.js", "browser-rage.js", "browser-rolls.js", "browser-zero-hp.js",
+  "browser-state.js", "browser-resources.js", "browser-resource-conversion.js", "browser-rage.js", "browser-rolls.js", "browser-zero-hp.js",
   "browser-graze.js", "browser-vex.js", "browser-attack-outcome.js", "browser-attack.js", "browser-reactions.js",
   "browser-dodge.js", "browser-saving-throws.js", "browser-saves.js", "browser-condition-lifecycle.js", "browser-charge.js",
   "browser-light-weapons.js", "browser-light-attack.js", "browser-standard-attack-action.js",
