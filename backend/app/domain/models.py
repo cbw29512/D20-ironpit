e969ebd2681ce0045fcf1890cfd32f055b6647b7
Chris Wilson <divclass01@gmail.com>
2026-09-26 15:07:29 -0400

    feat: export universal spell cast modifier schema

--- backend/app/domain/models.py
+++ backend/app/domain/models.py
@@ -96,6 +96,7 @@ __all__ = [
     "OnHitDamage",
     "PersistentSpellAttackAction",
     "PersistentSpellAttackState",
+    "ResourceBackedSpellSaveDisadvantage",
     "ResourceConversionAction",
     "ResourceDefinition",
     "ResourceState",
